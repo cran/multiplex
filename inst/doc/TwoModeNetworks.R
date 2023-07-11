1441730f37e02339ecae1458e36224789ecedf28
Antonio Rivero Ostoic <multiplex@post.com>
2023-07-11 08:40:14 +0000

    version 3.1.0

--- inst/doc/TwoModeNetworks.R
+++ inst/doc/TwoModeNetworks.R
@@ -1,5 +1,5 @@
 ## ----setup, include=FALSE-----------------------------------------------------------------------------------
-knitr::opts_chunk$set(size = 'footnotesize', background = '#FFFFFF', prompt = TRUE, strip.white = FALSE, comment = NA)
+knitr::opts_chunk$set(size = "footnotesize", background = "#FFFFFF", prompt = TRUE, strip.white = FALSE, comment = NA)
 options(width=110)
 knitr::knit_hooks$set(small.mar = function(before, options, envir) {
     if (before) par(mar = c(4, 4, .1, .1))  # smaller margin on top and right
@@ -21,38 +21,47 @@ library("multiplex")
 # Galois representation between objects and attributes
 galois(frt)
 
-## ----galoisReduc, echo=-2-----------------------------------------------------------------------------------
+## ----galoisReduc, echo=TRUE---------------------------------------------------------------------------------
 # Galois derivation with a reduced labeling
-gd <- galois(frt, labeling = "reduced")
 galois(frt, labeling = "reduced")
 
-## ----strgaloisReduc, size='scriptsize'----------------------------------------------------------------------
-str(gd$gc$full)
+## ----strgaloisReduc, size="scriptsize"----------------------------------------------------------------------
+galois(frt, labeling = "reduced") |> 
+  getElement("gc") |> 
+  getElement("full") |> 
+  str()
 
-## ----partialorder, echo=-3----------------------------------------------------------------------------------
+## ----partialorder, echo=-4----------------------------------------------------------------------------------
 # Partial ordering of the formal concepts with established labels
-pogdc <- partial.order(gd, type = "galois", lbs = paste("c", seq_len(length(gd$gc$full)), sep = ""))
+pogdc <- galois(frt, labeling = "reduced") |> 
+  partial.order(type = "galois", lbs = paste("c", seq_len(12), sep = ""))
 pogdc
 
-## ----pogc---------------------------------------------------------------------------------------------------
-# First we assign the partial order of the reduced context to 'pogd'
-pogd <- partial.order(gd, type = "galois")
-
-## ----diagrampogc, fig.pos='H', fig.width=4.5, fig.height=4.5, fig.align='center', fig.cap='Concept Lattice of fruits and color characteristics', echo=-1, small.mar=TRUE----
+## ----diagrampogc, fig.pos="H", fig.width=4.5, fig.height=4.5, fig.align="center", fig.cap="Concept Lattice of fruits and color characteristics", echo=-1, small.mar=TRUE----
 par(mar=c(0,0,0,0))
 # Plot the lattice diagram
-suppressPackageStartupMessages(library("Rgraphviz", quietly = TRUE))
-diagram(pogd, main = "Fruits & Colors", fsize = 17, fcol = "red", col.main = "blue")
+suppressPackageStartupMessages(require("Rgraphviz", quietly = TRUE))
+frt |> galois(labeling = "reduced") |> 
+  partial.order(type = "galois") |> 
+  diagram(main = "Fruits & Colors", fsize = 17, fcol = "red", col.main = "blue")
 
 ## ----diaglevels, echo=TRUE----------------------------------------------------------------------------------
-# Diagram levels
-if( require("Rgraphviz", quietly = TRUE, warn.conflicts = FALSE)) {
-diagram.levels(pogdc) }
+# Diagram levels using native pipeing
+require("Rgraphviz", quietly = TRUE, warn.conflicts = FALSE)
+frt |> 
+  galois(labeling = "reduced") |> 
+  partial.order(type = "galois") |> 
+  diagram.levels() 
 
 ## ----diaglevelsperm, echo=TRUE, message=FALSE, warning=FALSE------------------------------------------------
 # Diagram levels with permutation
 if( require("Rgraphviz", quietly = TRUE, warn.conflicts = FALSE)) {
 diagram.levels(pogdc, perm = TRUE) }
+
+## ----pogc, echo=TRUE----------------------------------------------------------------------------------------
+# First assign the partial order of the reduced context
+pogd <- frt |> galois(labeling = "reduced") |> 
+  partial.order(type = "galois")
 
 ## ----princfltrlbs, echo=TRUE--------------------------------------------------------------------------------
 # Principal order filter of the third concept 
@@ -70,7 +79,7 @@ fltr(c("Lemon", "Lime"), pogd)
 # Order ideal of two concepts
 fltr(c(9, 11), pogd, ideal = TRUE)
 
-## ----bipp, echo=TRUE, eval=TRUE, fig.pos='H', fig.width=3.0, fig.height=3.0, fig.align='center', fig.env='figure', fig.cap='Bipartite graph', small.mar=TRUE----
+## ----bipp, echo=TRUE, eval=TRUE, fig.pos="H", fig.width=3.0, fig.height=3.0, fig.align="center", fig.env="figure", fig.cap="Bipartite graph", small.mar=TRUE----
 # Load the "multigraph" package and plot bipartite graph
 suppressPackageStartupMessages(library("multigraph", quietly = TRUE))
 bmgraph(frt, pch = 16:15, fsize = 6)
@@ -79,6 +88,6 @@ bmgraph(frt, pch = 16:15, fsize = 6)
 #  # Plot proyection of bipartite network
 #  bmgraph(frt, layout = "force", seed = 1, cex = 3, fsize = 7, vcol = 8, pch = 16:15, rot = 25)
 
-## ----binp, fig.pos='H', fig.width=4.5, fig.height=4.5, fig.align='center', fig.env='figure', fig.cap='Bipartite graph with a force-directed layout', echo=FALSE, small.mar=TRUE----
+## ----binp, fig.pos="H", fig.width=4.5, fig.height=4.5, fig.align="center", fig.env="figure", fig.cap="Bipartite graph with a force-directed layout", echo=FALSE, small.mar=TRUE----
 bmgraph(frt, layout = "force", seed = 1, cex = 3, fsize = 7, vcol = 8, pch = 16:15, rot = 25)
 
